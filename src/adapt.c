0b298a2d08e8f7f9abc5b659c241d83d89255916
Jerome Humbert <djeedai@gmail.com>
2019-11-03 09:18:19 +0000

    Handle compile-time SSE support in adapt.c
    Check compile-time flags for assumed SSE support, which avoids having to
    do a runtime check.

--- src/adapt.c
+++ src/adapt.c
@@ -16,15 +16,25 @@ void FAR_API FAR_FN(adapt_ch1x2f32)(const float512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in * 2)
     return;
+#if defined(ENABLE_SSE)
+#if !defined(ASSUME_SSE)
   if (FAR_FN(has_sse)())  // currently uses only SSE, not SSE4.1
+#endif // !defined(ASSUME_SSE)
   {
     FAR_FN(adapt_ch1x2f32_sse41)
     ((const float128*)buf_in, size_in, sample_rate, (float128*)buf_out,
      size_out);
-  } else {
+  }
+#if !defined(ASSUME_SSE)
+  else
+#endif  // !defined(ASSUME_SSE)
+#endif  // defined(ENABLE_SSE)
+#if !defined(ASSUME_SSE)
+  {
     FAR_FN(adapt_ch1x2f32_fpu)
     ((const float*)buf_in, size_in, sample_rate, (float*)buf_out, size_out);
   }
+#endif  // !defined(ASSUME_SSE)
 }
 
 void FAR_API FAR_FN(adapt_ch2x1f32)(const float512* FAR_RESTRICT buf_in,
@@ -34,15 +44,25 @@ void FAR_API FAR_FN(adapt_ch2x1f32)(const float512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in / 2)
     return;
-  if (FAR_FN(has_sse41)())  // currently uses only SSE3, not SSE4.1
+#if defined(ENABLE_SSE3)
+#if !defined(ASSUME_SSE3)
+  if (FAR_FN(has_sse3)())  // currently uses only SSE3, not SSE4.1
+#endif // !defined(ASSUME_SSE3)
   {
     FAR_FN(adapt_ch2x1f32_sse41)
     ((const float128*)buf_in, size_in, sample_rate, (float128*)buf_out,
      size_out);
-  } else {
+  }
+#if !defined(ASSUME_SSE3)
+  else
+#endif  // !defined(ASSUME_SSE3)
+#endif  // defined(ENABLE_SSE3)
+#if !defined(ASSUME_SSE3)
+  {
     FAR_FN(adapt_ch2x1f32_fpu)
     ((const float*)buf_in, size_in, sample_rate, (float*)buf_out, size_out);
   }
+#endif  // !defined(ASSUME_SSE3)
 }
 
 void FAR_API FAR_FN(adapt_ch1x2s16)(const short512* FAR_RESTRICT buf_in,
@@ -52,15 +72,25 @@ void FAR_API FAR_FN(adapt_ch1x2s16)(const short512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in * 2)
     return;
+#if defined(ENABLE_SSE)
+#if !defined(ASSUME_SSE)
   if (FAR_FN(has_sse)())  // currently uses only SSE, not SSE4.1
+#endif  // !defined(ASSUME_SSE)
   {
     FAR_FN(adapt_ch1x2s16_sse41)
     ((const short128*)buf_in, size_in, sample_rate, (short128*)buf_out,
      size_out);
-  } else {
+  }
+#if !defined(ASSUME_SSE)
+  else
+#endif  // !defined(ASSUME_SSE)
+#endif  // defined(ENABLE_SSE)
+#if !defined(ASSUME_SSE)
+  {
     FAR_FN(adapt_ch1x2s16_fpu)
     ((const int16*)buf_in, size_in, sample_rate, (int16*)buf_out, size_out);
   }
+#endif  // !defined(ASSUME_SSE)
 }
 
 void FAR_API FAR_FN(adapt_ch2x1s16)(const short512* FAR_RESTRICT buf_in,
@@ -70,15 +100,25 @@ void FAR_API FAR_FN(adapt_ch2x1s16)(const short512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in / 2)
     return;
+#if defined(ENABLE_SSE)
+#if !defined(ASSUME_SSE)
   if (FAR_FN(has_sse)())  // currently uses only SSE, not SSE4.1
+#endif  // !defined(ASSUME_SSE)
   {
     FAR_FN(adapt_ch2x1s16_sse41)
     ((const short128*)buf_in, size_in, sample_rate, (short128*)buf_out,
      size_out);
-  } else {
+  }
+#if !defined(ASSUME_SSE)
+  else
+#endif  // !defined(ASSUME_SSE)
+#endif  // defined(ENABLE_SSE)
+#if !defined(ASSUME_SSE)
+  {
     FAR_FN(adapt_ch2x1s16_fpu)
     ((const int16*)buf_in, size_in, sample_rate, (int16*)buf_out, size_out);
   }
+#endif  // !defined(ASSUME_SSE)
 }
 
 #ifdef __cplusplus
