3971baaf1d7aeb6a09712ebc9389ed27784365f0
Jerome Humbert <djeedai@gmail.com>
2019-11-03 11:55:49 +0000

    Use tri-state CMake variable for SIMD options
    Define a tri-state option variable for SIMD support in CMake:
    - Disable: disable support for the SIMD feature entirely (compiled out).
    - RuntimeCheckEnabled: enable support for the SIMD feature, with runtime
    check for availability using CPUID. This is the default for most options
    and the safest.
    - CompileTimeAssumed: enable support for the SIMD feature, and assume
    compile-time support, directly using the CPU instructions. This must
    only be used with features that are assumed to always be present on the
    target device. This is the default for SSE and SSE2, which are old
    enough to be supported by any modern CPU.

--- src/adapt.c
+++ src/adapt.c
@@ -16,25 +16,25 @@ void FAR_API FAR_FN(adapt_ch1x2f32)(const float512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in * 2)
     return;
-#if defined(ENABLE_SSE)
-#if !defined(ASSUME_SSE)
+#if defined(FAR_ENABLE_SSE)
+#if !defined(FAR_ASSUME_SSE)
   if (FAR_FN(has_sse)())  // currently uses only SSE, not SSE4.1
-#endif // !defined(ASSUME_SSE)
+#endif // !defined(FAR_ASSUME_SSE)
   {
     FAR_FN(adapt_ch1x2f32_sse41)
     ((const float128*)buf_in, size_in, sample_rate, (float128*)buf_out,
      size_out);
   }
-#if !defined(ASSUME_SSE)
+#if !defined(FAR_ASSUME_SSE)
   else
-#endif  // !defined(ASSUME_SSE)
-#endif  // defined(ENABLE_SSE)
-#if !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
+#endif  // defined(FAR_ENABLE_SSE)
+#if !defined(FAR_ASSUME_SSE)
   {
     FAR_FN(adapt_ch1x2f32_fpu)
     ((const float*)buf_in, size_in, sample_rate, (float*)buf_out, size_out);
   }
-#endif  // !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
 }
 
 void FAR_API FAR_FN(adapt_ch2x1f32)(const float512* FAR_RESTRICT buf_in,
@@ -44,25 +44,25 @@ void FAR_API FAR_FN(adapt_ch2x1f32)(const float512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in / 2)
     return;
-#if defined(ENABLE_SSE3)
-#if !defined(ASSUME_SSE3)
+#if defined(FAR_ENABLE_SSE3)
+#if !defined(FAR_ASSUME_SSE3)
   if (FAR_FN(has_sse3)())  // currently uses only SSE3, not SSE4.1
-#endif // !defined(ASSUME_SSE3)
+#endif // !defined(FAR_ASSUME_SSE3)
   {
     FAR_FN(adapt_ch2x1f32_sse41)
     ((const float128*)buf_in, size_in, sample_rate, (float128*)buf_out,
      size_out);
   }
-#if !defined(ASSUME_SSE3)
+#if !defined(FAR_ASSUME_SSE3)
   else
-#endif  // !defined(ASSUME_SSE3)
-#endif  // defined(ENABLE_SSE3)
-#if !defined(ASSUME_SSE3)
+#endif  // !defined(FAR_ASSUME_SSE3)
+#endif  // defined(FAR_ENABLE_SSE3)
+#if !defined(FAR_ASSUME_SSE3)
   {
     FAR_FN(adapt_ch2x1f32_fpu)
     ((const float*)buf_in, size_in, sample_rate, (float*)buf_out, size_out);
   }
-#endif  // !defined(ASSUME_SSE3)
+#endif  // !defined(FAR_ASSUME_SSE3)
 }
 
 void FAR_API FAR_FN(adapt_ch1x2s16)(const short512* FAR_RESTRICT buf_in,
@@ -72,25 +72,25 @@ void FAR_API FAR_FN(adapt_ch1x2s16)(const short512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in * 2)
     return;
-#if defined(ENABLE_SSE)
-#if !defined(ASSUME_SSE)
+#if defined(FAR_ENABLE_SSE)
+#if !defined(FAR_ASSUME_SSE)
   if (FAR_FN(has_sse)())  // currently uses only SSE, not SSE4.1
-#endif  // !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
   {
     FAR_FN(adapt_ch1x2s16_sse41)
     ((const short128*)buf_in, size_in, sample_rate, (short128*)buf_out,
      size_out);
   }
-#if !defined(ASSUME_SSE)
+#if !defined(FAR_ASSUME_SSE)
   else
-#endif  // !defined(ASSUME_SSE)
-#endif  // defined(ENABLE_SSE)
-#if !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
+#endif  // defined(FAR_ENABLE_SSE)
+#if !defined(FAR_ASSUME_SSE)
   {
     FAR_FN(adapt_ch1x2s16_fpu)
     ((const int16*)buf_in, size_in, sample_rate, (int16*)buf_out, size_out);
   }
-#endif  // !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
 }
 
 void FAR_API FAR_FN(adapt_ch2x1s16)(const short512* FAR_RESTRICT buf_in,
@@ -100,25 +100,25 @@ void FAR_API FAR_FN(adapt_ch2x1s16)(const short512* FAR_RESTRICT buf_in,
                                     size_t size_out) {
   if (size_out < size_in / 2)
     return;
-#if defined(ENABLE_SSE)
-#if !defined(ASSUME_SSE)
+#if defined(FAR_ENABLE_SSE)
+#if !defined(FAR_ASSUME_SSE)
   if (FAR_FN(has_sse)())  // currently uses only SSE, not SSE4.1
-#endif  // !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
   {
     FAR_FN(adapt_ch2x1s16_sse41)
     ((const short128*)buf_in, size_in, sample_rate, (short128*)buf_out,
      size_out);
   }
-#if !defined(ASSUME_SSE)
+#if !defined(FAR_ASSUME_SSE)
   else
-#endif  // !defined(ASSUME_SSE)
-#endif  // defined(ENABLE_SSE)
-#if !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
+#endif  // defined(FAR_ENABLE_SSE)
+#if !defined(FAR_ASSUME_SSE)
   {
     FAR_FN(adapt_ch2x1s16_fpu)
     ((const int16*)buf_in, size_in, sample_rate, (int16*)buf_out, size_out);
   }
-#endif  // !defined(ASSUME_SSE)
+#endif  // !defined(FAR_ASSUME_SSE)
 }
 
 #ifdef __cplusplus
