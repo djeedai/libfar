1e6bafd21b5433747c44aec3be0a805ef584502a
Jerome Humbert <djeedai@gmail.com>
2019-11-03 10:31:42 +0000

    Add more memory unit tests

--- test/test_memory.cpp
+++ test/test_memory.cpp
@@ -1,16 +1,49 @@
 #include "pch.h"
 
 TEST(Memory, Alloc) {
-  const size_t align = (1 << 4);  // 16
-  void* ptr = libfar::malloc_align(64, align);
-  ASSERT_NE(nullptr, ptr);
-  ASSERT_EQ(0, (size_t)ptr & (align - 1));
-  libfar::free_align(ptr);
+  for (int size = 1; size <= 256; ++size) {
+    for (int align_exp = 2; align_exp <= 5; ++align_exp) {
+      const size_t align = (1 << align_exp);
+      void* ptr = libfar::malloc_align(size, align);
+      ASSERT_NE(nullptr, ptr);
+      ASSERT_EQ(0, (size_t)ptr & (align - 1));
+      libfar::free_align(ptr);
+    }
+  }
+}
+
+TEST(Memory, Alloc16) {
+  for (int size = 1; size <= 256; ++size) {
+    byte16* ptr = libfar::malloc_align16(size);
+    ASSERT_NE(nullptr, ptr);
+    ASSERT_EQ(0, (size_t)ptr & 15);
+    libfar::free_align16(ptr);
+  }
 }
 
 TEST(Memory, Alloc32) {
-  byte32* ptr = libfar::malloc_align32(64);
-  ASSERT_NE(nullptr, ptr);
-  ASSERT_EQ(0, (size_t)ptr & 31);
-  libfar::free_align32(ptr);
+  for (int size = 1; size <= 256; ++size) {
+    byte32* ptr = libfar::malloc_align32(size);
+    ASSERT_NE(nullptr, ptr);
+    ASSERT_EQ(0, (size_t)ptr & 31);
+    libfar::free_align32(ptr);
+  }
+}
+
+TEST(Memory, Alloc64) {
+  for (int size = 1; size <= 256; ++size) {
+    byte64* ptr = libfar::malloc_align64(size);
+    ASSERT_NE(nullptr, ptr);
+    ASSERT_EQ(0, (size_t)ptr & 63);
+    libfar::free_align64(ptr);
+  }
+}
+
+TEST(Memory, AllocTemplate) {
+  for (int size = 1; size <= 256; ++size) {
+    auto ptr = libfar::malloc_align<short128>(size);
+    ASSERT_NE(nullptr, ptr);
+    ASSERT_EQ(0, (size_t)ptr & 15);
+    libfar::free_align(ptr);
+  }
 }
